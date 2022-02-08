4a79a764ac0b47f2a0a0a1fd90bac0f9e0ca5869
Joey Castillo <joeycastillo@utexas.edu>
2022-02-08 16:41:01 -0500

    saml22: WIP setting up clocks

--- samd/saml22/clocks.c
+++ samd/saml22/clocks.c
@@ -32,21 +32,20 @@ bool gclk_enabled(uint8_t gclk) {
 }
 
 void disable_gclk(uint8_t gclk) {
-    while ((GCLK->SYNCBUSY.vec.GENCTRL & (1 << gclk)) != 0) {}
-    GCLK->GENCTRL[gclk].bit.GENEN = false;
-    while ((GCLK->SYNCBUSY.vec.GENCTRL & (1 << gclk)) != 0) {}
+    while (GCLK->SYNCBUSY.reg & (1 << (GCLK_SYNCBUSY_GENCTRL0_Pos + gclk)));
+    GCLK->GENCTRL[gclk].reg &= ~GCLK_GENCTRL_GENEN;
+    while (GCLK->SYNCBUSY.reg & (1 << (GCLK_SYNCBUSY_GENCTRL0_Pos + gclk)));
 }
 
 void connect_gclk_to_peripheral(uint8_t gclk, uint8_t peripheral) {
-    GCLK->PCHCTRL[peripheral].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN(gclk);
-    while(GCLK->SYNCBUSY.reg != 0) {}
+    hri_gclk_write_PCHCTRL_reg(GCLK, peripheral, gclk | GCLK_PCHCTRL_CHEN);
 }
 
 void disconnect_gclk_from_peripheral(uint8_t gclk, uint8_t peripheral) {
-    GCLK->PCHCTRL[peripheral].reg = 0;
+    hri_gclk_write_PCHCTRL_reg(GCLK, peripheral, gclk);
 }
 
-static void enable_clock_generator_sync(uint8_t gclk, uint32_t source, uint16_t divisor, bool sync) {
+void enable_clock_generator(uint8_t gclk, uint32_t source, uint16_t divisor) {
     uint32_t divsel = 0;
     // The datasheet says 8 bits and max value of 512, how is that possible?
     if (divisor > 255) { // Generator 1 has 16 bits
@@ -60,12 +59,6 @@ static void enable_clock_generator_sync(uint8_t gclk, uint32_t source, uint16_t 
     }
 
     GCLK->GENCTRL[gclk].reg = GCLK_GENCTRL_SRC(source) | GCLK_GENCTRL_DIV(divisor) | divsel | GCLK_GENCTRL_OE | GCLK_GENCTRL_GENEN;
-    if (sync)
-        while ((GCLK->SYNCBUSY.vec.GENCTRL & (1 << gclk)) != 0) {}
-}
-
-void enable_clock_generator(uint8_t gclk, uint32_t source, uint16_t divisor) {
-    enable_clock_generator_sync(gclk, source, divisor, true);
 }
 
 void disable_clock_generator(uint8_t gclk) {
@@ -73,51 +66,84 @@ void disable_clock_generator(uint8_t gclk) {
     while ((GCLK->SYNCBUSY.vec.GENCTRL & (1 << gclk)) != 0) {}
 }
 
+static void osc16minit_clock_source_osc16m(void) {
+    OSCCTRL->OSC16MCTRL.bit.ONDEMAND = 0;
+    OSCCTRL->OSC16MCTRL.bit.FSEL = 3;
+    OSCCTRL->OSC16MCTRL.bit.ENABLE = 1;
+    while (!OSCCTRL->STATUS.bit.OSC16MRDY) {}
+}
+
 static void init_clock_source_osculp32k(void) {
-    // Calibration value is loaded at startup
-    OSC32KCTRL->OSCULP32K.bit.EN1K = 0;
-    OSC32KCTRL->OSCULP32K.bit.EN32K = 1;
+    uint16_t calib = (OSC32KCTRL->OSCULP32K.reg & OSC32KCTRL_OSCULP32K_CALIB_Msk) >> OSC32KCTRL_OSCULP32K_CALIB_Pos;
+    OSC32KCTRL->OSCULP32K.reg = OSC32KCTRL_OSCULP32K_CALIB(calib);
 }
 
 static void init_clock_source_xosc32k(void) {
-    OSC32KCTRL->XOSC32K.reg = OSC32KCTRL_XOSC32K_ONDEMAND |
-                              OSC32KCTRL_XOSC32K_EN32K |
+    OSC32KCTRL->XOSC32K.reg = OSC32KCTRL_XOSC32K_EN32K |
                               OSC32KCTRL_XOSC32K_XTALEN |
                               OSC32KCTRL_XOSC32K_ENABLE;
+    while (!OSC32KCTRL->STATUS.bit.XOSC32KRDY) {}
 }
 
-static void init_clock_source_dpll0(void)
+// static void init_clock_source_dfll48m_xosc(void) {
+//     OSCCTRL->DFLLCTRL.reg = OSCCTRL_DFLLCTRL_ENABLE;
+//     while (!OSCCTRL->STATUS.bit.DFLLRDY) {}
+//     OSCCTRL->DFLLMUL.reg = OSCCTRL_DFLLMUL_CSTEP(0x1f / 4) |
+//                            OSCCTRL_DFLLMUL_FSTEP(0xff / 4) |
+//                            OSCCTRL_DFLLMUL_MUL(48000000 / 32768);
+//     uint32_t coarse = (*((uint32_t *)FUSES_DFLL48M_COARSE_CAL_ADDR) & FUSES_DFLL48M_COARSE_CAL_Msk) >> FUSES_DFLL48M_COARSE_CAL_Pos;
+//     if (coarse == 0x3f) {
+//         coarse = 0x1f;
+//     }
+//     OSCCTRL->DFLLVAL.reg = OSCCTRL_DFLLVAL_COARSE(coarse) |
+//                            OSCCTRL_DFLLVAL_FINE(512);
+
+//     OSCCTRL->DFLLCTRL.reg = 0;
+//     while (!OSCCTRL->STATUS.bit.DFLLRDY) {}
+//     OSCCTRL->DFLLCTRL.reg = OSCCTRL_DFLLCTRL_MODE |
+//                             OSCCTRL_DFLLCTRL_ENABLE;
+//     while (!OSCCTRL->STATUS.bit.DFLLRDY) {}
+
+//     // Wait for the fine lock on the DFLL.
+//     while (!OSCCTRL->STATUS.bit.DFLLLCKC || !OSCCTRL->STATUS.bit.DFLLLCKF) {}
+// }
+
+static void init_clock_source_dfll48m_usb(uint32_t fine_calibration) {
+    OSCCTRL->INTFLAG.reg = OSCCTRL_INTFLAG_DFLLRDY;
+    OSCCTRL->DFLLCTRL.reg = 0;
+    while (!(OSCCTRL->STATUS.reg & OSCCTRL_STATUS_DFLLRDY));
+
+    // set the coarse and fine values to speed up frequency lock.
+    uint32_t coarse =(*((uint32_t *)NVMCTRL_OTP5)) >> 26;
+    OSCCTRL->DFLLVAL.reg = OSCCTRL_DFLLVAL_COARSE(coarse) |
+                           OSCCTRL_DFLLVAL_FINE(0x200);
+    // set coarse and fine steps, and multiplier (48 MHz = 32768 Hz * 1465)
+    OSCCTRL->DFLLMUL.reg = OSCCTRL_DFLLMUL_CSTEP( 1 ) |
+                           OSCCTRL_DFLLMUL_FSTEP( 1 ) |
+                           OSCCTRL_DFLLMUL_MUL( 1465 );
+    // set closed loop mode, chill cycle disable and USB clock recovery mode, and enable the DFLL.
+    OSCCTRL->DFLLCTRL.reg = OSCCTRL_DFLLCTRL_MODE | OSCCTRL_DFLLCTRL_CCDIS | OSCCTRL_DFLLCTRL_ONDEMAND | OSCCTRL_DFLLCTRL_RUNSTDBY | OSCCTRL_DFLLCTRL_USBCRM | OSCCTRL_DFLLCTRL_ENABLE;
+    while (!(OSCCTRL->STATUS.reg & OSCCTRL_STATUS_DFLLRDY));
+}
+
+void clock_init(bool has_crystal, uint32_t dfll48m_fine_calibration)
 {
-    GCLK->PCHCTRL[OSCCTRL_GCLK_ID_FDPLL].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN(5);
-    OSCCTRL->DPLLRATIO.reg = OSCCTRL_DPLLRATIO_LDRFRAC(0) | OSCCTRL_DPLLRATIO_LDR(59);
-    OSCCTRL->DPLLCTRLB.reg = OSCCTRL_DPLLCTRLB_REFCLK(0);
-    OSCCTRL->DPLLCTRLA.reg = OSCCTRL_DPLLCTRLA_ENABLE;
-
-    while (!(OSCCTRL->DPLLSTATUS.bit.LOCK || OSCCTRL->DPLLSTATUS.bit.CLKRDY)) {}
-}
-
-void clock_init(bool has_crystal, uint32_t dfll48m_fine_calibration) {
-    // DFLL48M is enabled by default
-    // TODO: handle fine calibration data.
-
-    init_clock_source_osculp32k();
-
+    osc16minit_clock_source_osc16m();
     if (has_crystal) {
         init_clock_source_xosc32k();
-        OSC32KCTRL->RTCCTRL.bit.RTCSEL = OSC32KCTRL_RTCCTRL_RTCSEL_XOSC32K_Val;
     } else {
-        OSC32KCTRL->RTCCTRL.bit.RTCSEL = OSC32KCTRL_RTCCTRL_RTCSEL_ULP32K_Val;
+        init_clock_source_osculp32k();
     }
 
-    MCLK->CPUDIV.reg = MCLK_CPUDIV_CPUDIV(1);
+    init_clock_source_dfll48m_usb(dfll48m_fine_calibration);
 
-    enable_clock_generator_sync(0, GCLK_GENCTRL_SRC_DPLL96M_Val, 1, false);
-    enable_clock_generator_sync(1, GCLK_GENCTRL_SRC_DFLL48M_Val, 1, false);
-    enable_clock_generator_sync(4, GCLK_GENCTRL_SRC_DPLL96M_Val, 1, false);
-    enable_clock_generator_sync(5, GCLK_GENCTRL_SRC_DFLL48M_Val, 24, false);
-    enable_clock_generator_sync(6, GCLK_GENCTRL_SRC_DFLL48M_Val, 4, false);
-
-    init_clock_source_dpll0();
+    enable_clock_generator(0, GCLK_GENCTRL_SRC_OSC16M_Val, 1);
+    enable_clock_generator(1, GCLK_GENCTRL_SRC_DFLL48M_Val, 1);
+    if (has_crystal) {
+        enable_clock_generator(2, GCLK_GENCTRL_SRC_XOSC32K_Val, 1);
+    } else {
+        enable_clock_generator(2, GCLK_GENCTRL_SRC_OSCULP32K_Val, 1);
+    }
 
     // Do this after all static clock init so that they aren't used dynamically.
     init_dynamic_clocks();
@@ -143,64 +169,14 @@ static bool osc_enabled(uint8_t index) {
             return true;
         case GCLK_SOURCE_XOSC32K:
             return OSC32KCTRL->XOSC32K.bit.ENABLE;
+        case GCLK_SOURCE_OSC16M:
+            return OSCCTRL->OSC16MCTRL.bit.ENABLE;
         case GCLK_SOURCE_DFLL48M:
             return OSCCTRL->DFLLCTRL.bit.ENABLE;
         case GCLK_SOURCE_DPLL96M:
             return OSCCTRL->DPLLCTRLA.bit.ENABLE;
     };
     return false;
-}
-
-static uint32_t osc_get_source(uint8_t index) {
-    uint8_t dpll_index = index - GCLK_SOURCE_DPLL96M;
-    uint32_t refclk = OSCCTRL->DPLLCTRLB.bit.REFCLK;
-    switch (refclk) {
-        case 0x0:
-            return GCLK_SOURCE_XOSC32K;
-        case 0x1:
-            return GCLK_SOURCE_XOSC;
-        case 0x2:
-            return generator_get_source(GCLK->PCHCTRL[OSCCTRL_GCLK_ID_FDPLL + dpll_index].bit.GEN);
-    }
-    return 0;
-}
-
-static uint32_t osc_get_frequency(uint8_t index);
-
-static uint32_t generator_get_frequency(uint8_t gen) {
-        uint8_t src = GCLK->GENCTRL[gen].bit.SRC;
-        uint32_t div;
-        if (GCLK->GENCTRL[gen].bit.DIVSEL) {
-            div = 1 << (GCLK->GENCTRL[gen].bit.DIV + 1);
-        } else {
-            div = GCLK->GENCTRL[gen].bit.DIV;
-            if (!div)
-                div = 1;
-        }
-
-        return osc_get_frequency(src) / div;
-}
-
-static uint32_t dpll_get_frequency(uint8_t index) {
-    uint8_t dpll_index = index - GCLK_SOURCE_DPLL96M;
-    uint32_t refclk = OSCCTRL->DPLLCTRLB.bit.REFCLK;
-    uint32_t freq;
-
-    switch (refclk) {
-        case 0x0: // GCLK
-            freq = generator_get_frequency(GCLK->PCHCTRL[OSCCTRL_GCLK_ID_FDPLL + dpll_index].bit.GEN);
-            break;
-        case 0x1: // XOSC32
-            freq = 32768;
-            break;
-        case 0x2: // XOSC0
-        case 0x3: // XOSC1
-        default:
-            return 0; // unknown
-    }
-
-    return (freq * (OSCCTRL->DPLLRATIO.bit.LDR + 1)) +
-           (freq * OSCCTRL->DPLLRATIO.bit.LDRFRAC / 32);
 }
 
 static uint32_t osc_get_frequency(uint8_t index) {
@@ -210,10 +186,12 @@ static uint32_t osc_get_frequency(uint8_t index) {
         case GCLK_SOURCE_OSCULP32K:
         case GCLK_SOURCE_XOSC32K:
             return 32768;
+        case GCLK_SOURCE_OSC16M:
+            return 16000000;
         case GCLK_SOURCE_DFLL48M:
             return 48000000;
         case GCLK_SOURCE_DPLL96M:
-            return dpll_get_frequency(index);
+            return 96000000;
     }
     return 0;
 }
@@ -229,42 +207,31 @@ bool clock_get_enabled(uint8_t type, uint8_t index) {
 }
 
 bool clock_get_parent(uint8_t type, uint8_t index, uint8_t *p_type, uint8_t *p_index) {
-    if (type == 0 && osc_enabled(index)) {
-         if (index == GCLK_SOURCE_DPLL96M) {
-            *p_type = 0;
-            *p_index = osc_get_source(index);
-            return true;
-         }
-         return false;
-    }
-    if (type == 1 && index <= 47 && clk_enabled(index)) {
+    if (type == 1 && index <= 0x24 && clk_enabled(index)) {
         *p_type = 0;
         *p_index = generator_get_source(clk_get_generator(index));
         return true;
     }
-    if (type == 2) {
-        switch (index) {
-            case 0:
-            case 1:
-                *p_type = 0;
-                *p_index = generator_get_source(0);
-                return true;
-            case 2:
-                *p_type = 0;
-                switch (OSC32KCTRL->RTCCTRL.bit.RTCSEL) {
-                    case 0:
-                    case 1:
-                        *p_index = GCLK_SOURCE_OSCULP32K;
-                        return true;
-                    case 4:
-                    case 5:
-                        *p_index = GCLK_SOURCE_XOSC32K;
-                        return true;
-                }
-                return false;
-        }
+    if (type == 2 && index == 0) {
+        *p_type = 0;
+        *p_index = generator_get_source(0);
+        return true;
     }
     return false;
+}
+
+static uint32_t generator_get_frequency(uint8_t gen) {
+    uint8_t src = GCLK->GENCTRL[gen].bit.SRC;
+    uint32_t div;
+    if (GCLK->GENCTRL[gen].bit.DIVSEL) {
+        div = 1 << (GCLK->GENCTRL[gen].bit.DIV + 1);
+    } else {
+        div = GCLK->GENCTRL[gen].bit.DIV;
+        if (!div)
+            div = 1;
+    }
+
+    return osc_get_frequency(src) / div;
 }
 
 uint32_t clock_get_frequency(uint8_t type, uint8_t index) {
